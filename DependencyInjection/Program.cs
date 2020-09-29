b0ec8c522173f87cc2c82a38b4611d193dc0e32f
Mark McWhirter <mark.mcwhirter@gmail.com>
2020-09-28 22:34:09 -0700

    Add comments

--- DependencyInjection/Program.cs
+++ DependencyInjection/Program.cs
@@ -7,12 +7,12 @@ namespace DependencyInjection
     {
         static void Main(string[] args)
         {
-            var collection = new ServiceCollection();
-            collection.AddSingleton<IDemoService, DemoService>();
+            var collection = new ServiceCollection();   // Must have this first to instantiate collection
+            collection.AddSingleton<IDemoService, DemoService>();   // add a class with interface
             collection.AddScoped<IDemoService1, DemoService1>();
-            collection.AddScoped<IParse, Parse>();
+            collection.AddScoped<IParse, Parse>();  // make sure chained dependencies are added in reverse order
 
-            IServiceProvider serviceProvider = collection.BuildServiceProvider();
+            IServiceProvider serviceProvider = collection.BuildServiceProvider();       // last call to build collection
 
             //var service = serviceProvider.GetService<IDemoService>();
             //var service1 = serviceProvider.GetService<IDemoService1>();
@@ -20,9 +20,9 @@ namespace DependencyInjection
             //Console.WriteLine(service1.DooWahkaDo());
 
 
-            var service = serviceProvider.GetService<IParse>();
+            var service = serviceProvider.GetService<IParse>(); // create service provider reference
 
-            service.DoParse();
+            service.DoParse();  // call the method!
 
             if (serviceProvider is IDisposable)
             {
